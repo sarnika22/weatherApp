cd70a0b97bf61ea838950a770c6581527a739b90
Gokulakrishnan M <82328858+gokul-1998@users.noreply.github.com>
2024-04-26 05:50:45 +0000

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Weather from './components/search';
+import Weather from './components/weather';
 
 function App() {
   return (
     <div className="App">
-      <Weather/>
+     <Weather/>
     </div>
   );
 }
